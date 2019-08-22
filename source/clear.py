0a7b1bcc360cb82ca6f971d781426ac265d3eab4
Your Name2 <id791ecg@studenti.univr.it>
2019-08-22 11:55:27 +0200

    clear

--- source/clear.py
+++ source/clear.py
@@ -2,9 +2,10 @@ import os
 import shutil
 
     
-if os.path.exists("../features") and  input("Delete features? y/n") == "y":
+if os.path.exists("../features") and  input("Delete features? y/n: ") == "y":
     shutil.rmtree("../features")
 
-if os.path.exists("../models") and input("Delete models? y/n")=="y":
+if os.path.exists("../models") and input("Delete models? y/n: ")=="y":
     shutil.rmtree("../models")
 
+print("done")
