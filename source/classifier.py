478c1ec8490ca80d78df2f2c2c04ab7d63ace64a
Your Name2 <id791ecg@studenti.univr.it>
2019-12-06 10:37:14 +0100

    cia

--- source/classifier.py
+++ source/classifier.py
@@ -93,22 +93,19 @@ with open('../models/labels_' + net_name+ '.txt') as f:
     labels_names = [os.path.splitext(line.strip().split('$')[0])[0] for line in f]
 print(labels_names)
 
+print("Extracting features Using "+net_name +" net")
+extractor_model = modelClass(weights='imagenet', include_top=False)
+
 if crop_mode=='all':
-    extractor_model_all={}
-    model_all={}
+    model_all_svm={}
     for name in ["full", "top-left", "top-right", "bottom-left", "bottom-right"]:
-        print("Extracting features Using "+net_name + '_' + name +" net")
-        extractor_model_all[name] = modelClass(weights='imagenet', include_top=False)
         print("Loading model " + net_name)
-        model_all[name] = joblib.load('../models/'+net_name+ '_' + name +'.model')
+        model_all_svm[name] = joblib.load('../models/'+net_name+ '_' + name +'.model')
 
 else:
-    print("Extracting features Using "+net_name + '_' + crop_mode +" net")
-    extractor_model = modelClass(weights='imagenet', include_top=False)
-    
-
     print("Loading model " + net_name)
     model = joblib.load('../models/'+net_name+ '_' + crop_mode +'.model')
+
 for dirname in os.listdir(test_path):
     for fname in os.listdir(test_path + dirname):
 
@@ -120,21 +117,22 @@ for dirname in os.listdir(test_path):
         img_path = test_path + dirname + "/" + fname 
         img = image.load_img(img_path)
 
-
         if crop_mode=="all":
-            labels_found=[0]*len(labels_names["full"])
+            #labels found for all crop
+            labels_found=[0]*len(labels_names)
             max_p=0
             label_max=0
-            label_truth = labels_names_all["full"].index(dirname)
+            label_truth = labels_names.index(dirname)
             y_true.append(label_truth) 
             for name in ["full", "top-left", "top-right", "bottom-left", "bottom-right"]:
-                mode = crop_modes[name]
-                crop_x1 = mode[0]
-                crop_y1 = mode[1]
-                crop_x2 = mode[2]
-                crop_y2 = mode[3]
-                if(crop_mode != "full"): 
-                        img = img.crop((crop_x1 * img.width, crop_y1 * img.height, crop_x2 * img.width, crop_y2 * img.height,)) 
+
+                if(name != "full"): 
+                    mode = crop_modes[name]
+                    crop_x1 = mode[0]
+                    crop_y1 = mode[1]
+                    crop_x2 = mode[2]
+                    crop_y2 = mode[3]    
+                    img = img.crop((crop_x1 * img.width, crop_y1 * img.height, crop_x2 * img.width, crop_y2 * img.height,)) 
                 
                 img = img.resize(input_size)
                 img_data = image.img_to_array(img)
@@ -142,20 +140,22 @@ for dirname in os.listdir(test_path):
                 img_data = preprocess_function(img_data)
                 
                 features = extractor_model.predict(img_data).flatten() 
-
-                
-                label_predicted = model.predict(np.asmatrix(features)) 
-                labels_found[int(labels_predicted)]+=1
-
-                probability=model.predict_proba(np.asmatrix(features)) 
+ 
+                label_predicted = model_all_svm[name].predict(np.asmatrix(features)) 
+                probability=model_all_svm[name].predict_proba(np.asmatrix(features)) 
+                #if probability>0.9:
+                labels_found[int(label_predicted)]+=np.amax(probability)
+                #next print: for each model print probability maxfound for crop
+                #print( fname + " | predicted: " + str(labels_names[int(label_predicted)])+"probab: "+str(np.amax(probability)))
 
                 #if max_p < probability: commentato max probability
                 #    max_p= probability
-                #    label_max =int(labels_predicted)
+                #    label_max =int(label_predicted)
 
- 
-            #y_pred.append(label_predicted)
             label_predicted=np.argmax(labels_found)
+
+            print(labels_names)
+            print('probab found:'+str(labels_found))
             
             #label_predicted=label_max commento prob
             y_pred.append(label_predicted)
@@ -166,6 +166,7 @@ for dirname in os.listdir(test_path):
                 print("[ ] " + fname + " | predicted: " + str(labels_names[int(label_predicted)]))
                 freport.write(fname + " | predicted: " + str(labels_names[int(label_predicted)]) + "\n")
             count += 1
+            print('\n\n')
 
         else:
        
